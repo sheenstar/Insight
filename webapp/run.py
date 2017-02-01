76afb7ce96921e0612c89d46fc86088554099a5b
sheenstar <sharmasheena@gmail.com>
2017-02-01 09:16:08 -0800

    gridsearch

---
+++ webapp/run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import TextField, IntegerField, StringField, SubmitField, SelectField, DecimalField
+from wtforms.validators import Required, Optional
+import numpy as np
+import pandas as pd
+
+from flaskexample import app
+app.run(debug = True)
+
+
+
+
+
+
